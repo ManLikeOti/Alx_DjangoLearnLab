143f5d005ac3a75c8a44f81ad05b95e57be0ca37
ManLikeOti <anigboguotuto@gmail.com>
2025-02-23 22:52:36 +0100

    django deepdive

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.detail import DetailView
+from .models import Library
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from django.forms import BookForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login  
+from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
+
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library  # Specify the model
+    template_name = 'relationship_app/library_detail.html'  # Specify the template
+    context_object_name = 'library'
+
