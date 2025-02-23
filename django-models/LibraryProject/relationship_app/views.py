20f7e49c5f84c9df1fd6a707060fd7f91205cc51
ManLikeOti <anigboguotuto@gmail.com>
2025-02-23 23:03:21 +0100

    django deepdive

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,3 +20,16 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'  # Specify the template
     context_object_name = 'library'
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Save the user
+            login(request, user)  # Log the user in
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('list_books')  # Redirect to a desired page after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
