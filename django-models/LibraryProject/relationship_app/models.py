35fb5e758d6697c3f531127d220bffe4f45a3a71
ManLikeOti <anigboguotuto@gmail.com>
2025-02-25 20:37:20 +0100

    django deep work

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,45 @@
 from django.db import models
 
 # Create your models here.
-class 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+                return self.name
+        
+    class Book(models.Model):
+        title = models.CharField(max_length=200)
+        author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    
+        def __str__(self):
+            return self.title
+    
+        class Meta:
+            permissions = [
+                ("can_add_book", "Can add book"),
+                ("can_change_book", "Can change book"),
+                ("can_delete_book", "Can delete book"),
+            ]
+    
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
